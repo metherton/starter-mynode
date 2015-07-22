36cf422c3823757e7dd6cb347ca1b6033bf971f5
metherton <martinetherton63@googlemail.com>
2015-07-22 23:04:24 +0200

    use method from tutorial

--- sendsms.js
+++ sendsms.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var twilio = require('twilio');
+//var twilio = require('twilio');
 
 // Load configuration information from system environment variables.
 var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
@@ -8,12 +8,31 @@ var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
     TWILIO_NUMBER = process.env.TWILIO_NUMBER;
 
 // Create an authenticated client to access the Twilio REST API
-var client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+//var client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+//
+//
+//try {
+//    client.sendSms({
+//        to: '+31624543741',
+//        from: TWILIO_NUMBER,
+//        body: 'Erna please?! I love you'
+//    }, function(err, message) {
+//        process.stdout.write(message.sid);
+//    });
+//}
 
-client.sendSms({
-    to: '+31624543741',
-    from: TWILIO_NUMBER,
-    body: 'Erna please?! I love you'
-}, function(err, message) {
-    process.stdout.write(message.sid);
+
+var twilio = require('twilio');
+var client = new twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+client.sms.messages.create({
+    to:'+31624543741',
+    from:TWILIO_NUMBER,
+    body:'Daisy Daisy give me your answer do'
+}, function(error, message) {
+    if (error) {
+        console.log(error.message);
+    } else {
+        process.stdout.write(message.sid);
+    }
 });
+
