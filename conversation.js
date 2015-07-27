5d6ec0ca452f4d04dee825cf5b0410ba30fee0e8
metherton <martinetherton63@googlemail.com>
2015-07-27 11:41:37 +0200

    change message to sms

---
+++ conversation.js
@@ -0,0 +1,31 @@
+var http = require('http'),
+    express = require('express'),
+    twilio = require('twilio'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', function(req, res) {
+    var twilio = require('twilio');
+    var twiml = new twilio.TwimlResponse();
+    console.log(req.cookies.count);
+    var counter = parseInt(req.cookies.counter) || 0;
+
+    if (counter == 0) {
+        twiml.sms("Hello, thanks for the new message.");
+    } else {
+        twiml.sms("Hello, thanks for message number " + counter);
+    }
+    counter = counter + 1;
+    res.cookie('counter',counter);
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+
+http.createServer(app).listen(1337, function () {
+    console.log("Express server listening on port 1337");
+});
