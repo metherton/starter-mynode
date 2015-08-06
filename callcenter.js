610967fdaf8aff4496b7b9802204c53a02a86bdc
metherton <martinetherton63@googlemail.com>
2015-08-06 11:23:47 +0200

    aadded callcenter.js

--- callcenter.js
+++ callcenter.js
@@ -35,64 +35,28 @@ app.get('/', function(request, response) {
     response.render('index');
 });
 
-app.post('/sendsms1', function(request, response) {
-    client.sendSms({
-        to: '+31624543741',
-        from: TWILIO_NUMBER,
-        body: 'Twilio Doer test 1!'
-    }, function(err, data) {
-        response.send('Message sendsms1 is really inbound!');
-    });
+app.post('/waitmusic', function(request, response) {
+
+    var twiml = new twilio.TwimlResponse();
+
+    var queuePosition = request.body.QueuePosition;
+    var waitTime = request.body.AvgQueueTime;
+
+    twiml.say('Hello, you are caller  ' + queuePosition + ' in line. There is an average wait time of ' + waitTime + ' .');
+    twiml.play('http://com.twilio.sounds.music.s3.amazonaws.com/MARKOVICHAMP-Borghestral.mp3');
+
+    twiml.redirect('/waitmusic');
+    // Return an XML response to this request
+    response.set('Content-Type','text/xml');
+    response.send(twiml.toString());
+
 });
 
-app.post('/sendsms2', function(request, response) {
-    client.sendSms({
-        to: '+31624543741',
-        from: TWILIO_NUMBER,
-        body: 'Twilio Doer test 1!'
-    }, function(err, data) {
-        response.send('Message sendsms1 is really inbound!');
-    });
-});
-
-
-// handle a POST request to send a text message.  This is sent via ajax on our
-// home page
-app.post('/message', function(request, response) {
-    // Use the REST client to send a text message
-    client.sendSms({
-        to: request.param('to'),
-        from: TWILIO_NUMBER,
-        body: 'Have fun with your Twilio development!'
-    }, function(err, data) {
-        // When we get a response from Twilio, respond to the HTTP POST request
-        response.send('Message is inbound!');
-    });
-});
-
-// handle a POST request to make an outbound call.  This is sent via ajax on our
-// home page
-app.post('/call', function(request, response) {
-    // Use the REST client to send a text message
-    client.makeCall({
-        to: request.param('to'),
-        from: TWILIO_NUMBER,
-        url: 'http://twilio-elearning.herokuapp.com/starter/voice.php'
-    }, function(err, data) {
-        // When we get a response from Twilio, respond to the HTTP POST request
-        response.send('Call incoming!');
-    });
-});
-
-
-
-
-
-app.get('/hello', function(request, response) {
+app.get('/callers', function(request, response) {
     // Create a TwiML generator
     var twiml = new twilio.TwimlResponse();
-    twiml.say('Hello');
-    twiml.redirect('/step2')
+
+    twiml.enqueue({waitUrl: 'waitmusic'}, 'support');
     // Return an XML response to this request
     response.set('Content-Type','text/xml');
     response.send(twiml.toString());
